3f692a4a6733cf84105de936fc221dbab844592f
spartanrex10 <46965967+spartanrex10@users.noreply.github.com>
2019-02-05 09:29:34 -0600

    Changes (1)
    I added the sword to StartGame.java
    I managed to get Attack speed to print out 2 decimals instead of 100.

--- StartGame.java
+++ StartGame.java
@@ -12,7 +12,14 @@ public class StartGame {
     System.out.println(player1.getName() + " is at: " + player1.getHealth() + "Hp");
     System.out.println("Player2 name is: " + player2.getName());
     System.out.println(player2.getName() + " is at: " + player2.getHealth() + "Hp");
-    // this won't work until you create a getHealth method in your object. Uncomment it when you do.
+    
+    Sword sword1 = new Sword();
+    sword1.setName("Longsword");
+    
+    System.out.println(player1.getName() + " is using; a " + sword1.getName());
+    System.out.print("The " + sword1.getName() + " deals " + sword1.getDamage() + "Dmg. The " + sword1.getName() + " has an attack speed of: ");
+    System.out.printf("%.2f", sword1.getAttackSpeed());
+    System.out.println("/s");
     // System.out.println(player1.getName() + "'s health is: " + player1.getHealth());
   }  
   
--- Sword.java
+++ Sword.java
@@ -3,11 +3,38 @@
  private int damage;
  private boolean enchanted;
  private float attackSpeed;
-
+//Setters
  Sword() {
    this.damage = (int)(Math.random() * 15 + 1);
    this.attackSpeed = (float)(Math.random() * 3 + 1);
-   
-    // int random = (int)(Math.random() * 50 + 1);
  }
+   //Sword name setter
+   public void setName(String swordName) { 
+
+    // first let's make sure the length of the name is longer than 0
+    if (swordName.length() < 1) {
+      this.name = "invalid";
+      return;
+    }
+    
+    this.name = swordName;
+  }
+   //Getters
+   public int getDamage() {
+    return this.damage;
+   }
+   public float getAttackSpeed() {
+     return this.attackSpeed;
+   }
+  public String getName() {
+    return this.name;
+  }
+       //Hope, this will prevent ridiculously long decimals.
+     /* if (playerName.length() < 1) {
+      this.name = "invalid";
+      return;
+      
+      System.out.printf("%.2f", val);
+    }*/
+    // int random = (int)(Math.random() * 50 + 1);
  }
